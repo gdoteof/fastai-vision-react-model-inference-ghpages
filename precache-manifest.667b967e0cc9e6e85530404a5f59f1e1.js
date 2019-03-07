532a18343a0553c154803262363b60c9cd013678
Geoff Golder <geoffgolder@gmail.com>
2019-03-07 13:18:31 -0500

    Updates

---
+++ precache-manifest.667b967e0cc9e6e85530404a5f59f1e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fastai-vision-react-model-inference-ghpages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cc35f4ce1ed85b07c64d",
+    "url": "/fastai-vision-react-model-inference-ghpages/static/js/runtime~main.96935697.js"
+  },
+  {
+    "revision": "c4233c5bc0daf9cf9764",
+    "url": "/fastai-vision-react-model-inference-ghpages/static/js/main.f58ddea9.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/fastai-vision-react-model-inference-ghpages/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "c4233c5bc0daf9cf9764",
+    "url": "/fastai-vision-react-model-inference-ghpages/static/css/main.0801b16a.chunk.css"
+  },
+  {
+    "revision": "d1ec659a491a8bef8e7cfcd29f9123a2",
+    "url": "/fastai-vision-react-model-inference-ghpages/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gdoteof/fastai-vision-react-model-inference-ghpages/precache-manifest.42a3b393e70f792072556a025ea35426.js"
+  "/fastai-vision-react-model-inference-ghpages/precache-manifest.667b967e0cc9e6e85530404a5f59f1e1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/gdoteof/fastai-vision-react-model-inference-ghpages/index.html", {
+workbox.routing.registerNavigationRoute("/fastai-vision-react-model-inference-ghpages/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
