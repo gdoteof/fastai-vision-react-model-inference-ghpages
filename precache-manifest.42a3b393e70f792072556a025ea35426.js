2a6c5cf48eb14bff00e864350aa705668cd6b87e
Geoff Golder <geoffgolder@gmail.com>
2019-03-07 13:06:12 -0500

    Updates

---
+++ precache-manifest.42a3b393e70f792072556a025ea35426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gdoteof/fastai-vision-react-model-inference-ghpages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "da5892269e684ade347d",
+    "url": "/gdoteof/fastai-vision-react-model-inference-ghpages/static/js/runtime~main.ccc7299f.js"
+  },
+  {
+    "revision": "3789cf142eee8b6d484f",
+    "url": "/gdoteof/fastai-vision-react-model-inference-ghpages/static/js/main.051b2b01.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/gdoteof/fastai-vision-react-model-inference-ghpages/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "3789cf142eee8b6d484f",
+    "url": "/gdoteof/fastai-vision-react-model-inference-ghpages/static/css/main.0801b16a.chunk.css"
+  },
+  {
+    "revision": "638b4f2b83d9e4332744843ddbec6ece",
+    "url": "/gdoteof/fastai-vision-react-model-inference-ghpages/index.html"
+  }
+];
